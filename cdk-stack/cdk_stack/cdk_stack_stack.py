d9e56e6f5ec021e838bdad8ab23adeaed2029650
Robby Siewert <robbysiewert72@gmail.com>
2024-08-03 12:55:58 -0700

    style: cleanup

--- cdk-stack/cdk_stack/cdk_stack_stack.py
+++ cdk-stack/cdk_stack/cdk_stack_stack.py
@@ -1,7 +1,5 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
     aws_s3 as s3,
     RemovalPolicy
 )
@@ -12,13 +10,6 @@ class CdkStackStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkStackQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
-
+        # Example bucket to confirm successful deployment
         bucket = s3.Bucket(self, "MyBucket",
             removal_policy=RemovalPolicy.DESTROY)
