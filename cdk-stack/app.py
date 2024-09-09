07da293ff7bdfdd475415e787912b29d6fc36ff9
Robby Siewert <robbysiewert72@gmail.com>
2024-09-09 12:16:26 -0700

    fix: code_star_id

--- cdk-stack/app.py
+++ cdk-stack/app.py
@@ -5,10 +5,10 @@ from cdk_stack.pipeline_stack import CicdPipelineStack
 
 app = cdk.App()
 
-# CI/CD pipeline stack
-CicdPipelineStack(app, "CicdPipelineStack",
-    code_star_id="8e417a13-4164-4ce4-b1bf-deb77c7c6018", # TODO
-    )
+# # CI/CD pipeline stack
+# CicdPipelineStack(app, "CicdPipelineStack",
+#     code_star_id="8e417a13-4164-4ce4-b1bf-deb77c7c6018", # TODO
+#     )
 
 # Application PreProd stack
 CdkStackStack(app, "PreProdApplicationStack",
